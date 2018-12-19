3eb00409e7c0195019fe9271194ca547a016c962
Gaurang_Savaliya <gaurangsavaliya8@gmail.com>
2018-12-19 16:02:01 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,14 +3,14 @@ if(!session_id())
 	session_start();
 require_once 'Facebook/autoload.php';
 
-$app_id='453697758442568';
-$app_secret='3df6b2164cf31fb90951e5591ce1c197';
+$app_id='{app-id}'; // Replace {app-id} with your app id
+$app_secret='{app-secret}'; // Replace {app-secret} with your app secret
 $permissions = ['email,user_photos']; // Optional permissions
 $callbackUrl='https://gaurangsavaliya4.000webhostapp.com/callback.php';
 $logoutURL='https://gaurangsavaliya4.000webhostapp.com/logout.php';
 
 $fb = new Facebook\Facebook([
-  'app_id' => $app_id, // Replace {app-id} with your app id
+  'app_id' => $app_id, 
   'app_secret' => $app_secret,
   'default_graph_version' => 'v3.1'
   ]);
